fb4c042441b20a7f234350456e73f711ed24f5a3
Abigail Kane <aekane403@gmail.com>
2018-07-24 19:02:19 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,17 @@
+# build a program that returns Fizz if #/3 buzz if #/5 and fizzbuzz if /3 & /5
+
+def fizzbuzz(number)
+  if number % 5 == 0 && number % 3 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  end
+  
+end  
+
+puts fizzbuzz(3)
+puts fizzbuzz(7)
+puts fizzbuzz(10)
+puts fizzbuzz(15)
