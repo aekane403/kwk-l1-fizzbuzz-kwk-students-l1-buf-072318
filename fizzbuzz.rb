0016f655045223f9c46a4bee27b12babb4a2b58b
Abigail Kane <aekane403@gmail.com>
2018-07-24 18:48:21 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,11 @@
+# build a program that returns Fizz if #/3 buzz if #/5 and fizzbuzz if /3 & /5
+
+def fizzbuzz(number)
+  if number % 3 == 0
+    "Fizz"
+  end
+  
+end  
+
+puts fizzbuzz(3)
+puts fizzbuzz(7)
