cea110ba5ec6c7221ff03ade8dbdb3845354fef3
Abigail Kane <aekane403@gmail.com>
2018-07-24 18:57:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,15 @@
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
+  elsif number % 5 == 0 
+    "Buzz"
+  elsif number % 5 && number % 3 == 0
+    "Fizzbuzz"
   end
   
 end  
 
 puts fizzbuzz(3)
 puts fizzbuzz(7)
+puts fizzbuzz(10)
+puts fizzbuzz(15)
